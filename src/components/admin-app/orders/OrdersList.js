de7ac8d19194b768c1f2b098debb3010e4de4a69
Radu Codeci <radu.codeci@roweb.ro>
2021-01-11 22:08:00 +0200

    progress

--- src/components/admin-app/orders/OrdersList.js
+++ src/components/admin-app/orders/OrdersList.js
@@ -33,19 +33,19 @@ const OrdersList = () => {
 
   const CONTENT_HEIGHT = window.innerHeight;
 
-  const [products, setProducts] = useState(null);
-  const [productDialog, setProductDialog] = useState(false);
-  const [deleteProductDialog, setDeleteProductDialog] = useState(false);
-  const [deleteProductsDialog, setDeleteProductsDialog] = useState(false);
-  const [product, setProduct] = useState(emptyProduct);
-  const [selectedProducts, setSelectedProducts] = useState(null);
+  const [orders, setOrders] = useState(null);
+  const [orderDialog, setProductDialog] = useState(false);
+  const [deleteOrderDialog, setDeleteProductDialog] = useState(false);
+  const [deleteOrdersDialog, setDeleteProductsDialog] = useState(false);
+  const [order, setOrder] = useState(emptyProduct);
+  const [selectedOrders, setSelectedOrders] = useState(null);
   const [submitted, setSubmitted] = useState(false);
   const [globalFilter, setGlobalFilter] = useState(null);
   const toast = useRef(null);
   const dt = useRef(null);
 
   useEffect(() => {
-    setProducts(
+    setOrders(
       JSON.parse(
         '[{"id":"1000","code":"f230fh0g3","name":"Bamboo Watch","description":"Product Description","image":"bamboo-watch.jpg","price":65,"category":"Accessories","quantity":24,"inventoryStatus":"INSTOCK","rating":5},{"id":"1001","code":"nvklal433","name":"Black Watch","description":"Product Description","image":"black-watch.jpg","price":72,"category":"Accessories","quantity":61,"inventoryStatus":"INSTOCK","rating":4},{"id":"1002","code":"zz21cz3c1","name":"Blue Band","description":"Product Description","image":"blue-band.jpg","price":79,"category":"Fitness","quantity":2,"inventoryStatus":"LOWSTOCK","rating":3},{"id":"1003","code":"244wgerg2","name":"Blue T-Shirt","description":"Product Description","image":"blue-t-shirt.jpg","price":29,"category":"Clothing","quantity":25,"inventoryStatus":"INSTOCK","rating":5},{"id":"1004","code":"h456wer53","name":"Bracelet","description":"Product Description","image":"bracelet.jpg","price":15,"category":"Accessories","quantity":73,"inventoryStatus":"INSTOCK","rating":4},{"id":"1005","code":"av2231fwg","name":"Brown Purse","description":"Product Description","image":"brown-purse.jpg","price":120,"category":"Accessories","quantity":0,"inventoryStatus":"OUTOFSTOCK","rating":4},{"id":"1006","code":"bib36pfvm","name":"Chakra Bracelet","description":"Product Description","image":"chakra-bracelet.jpg","price":32,"category":"Accessories","quantity":5,"inventoryStatus":"LOWSTOCK","rating":3},{"id":"1007","code":"mbvjkgip5","name":"Galaxy Earrings","description":"Product Description","image":"galaxy-earrings.jpg","price":34,"category":"Accessories","quantity":23,"inventoryStatus":"INSTOCK","rating":5},{"id":"1008","code":"vbb124btr","name":"Game Controller","description":"Product Description","image":"game-controller.jpg","price":99,"category":"Electronics","quantity":2,"inventoryStatus":"LOWSTOCK","rating":4},{"id":"1009","code":"cm230f032","name":"Gaming Set","description":"Product Description","image":"gaming-set.jpg","price":299,"category":"Electronics","quantity":63,"inventoryStatus":"INSTOCK","rating":3},{"id":"1010","code":"plb34234v","name":"Gold Phone Case","description":"Product Description","image":"gold-phone-case.jpg","price":24,"category":"Accessories","quantity":0,"inventoryStatus":"OUTOFSTOCK","rating":4},{"id":"1011","code":"4920nnc2d","name":"Green Earbuds","description":"Product Description","image":"green-earbuds.jpg","price":89,"category":"Electronics","quantity":23,"inventoryStatus":"INSTOCK","rating":4},{"id":"1012","code":"250vm23cc","name":"Green T-Shirt","description":"Product Description","image":"green-t-shirt.jpg","price":49,"category":"Clothing","quantity":74,"inventoryStatus":"INSTOCK","rating":5},{"id":"1013","code":"fldsmn31b","name":"Grey T-Shirt","description":"Product Description","image":"grey-t-shirt.jpg","price":48,"category":"Clothing","quantity":0,"inventoryStatus":"OUTOFSTOCK","rating":3},{"id":"1014","code":"waas1x2as","name":"Headphones","description":"Product Description","image":"headphones.jpg","price":175,"category":"Electronics","quantity":8,"inventoryStatus":"LOWSTOCK","rating":5},{"id":"1015","code":"vb34btbg5","name":"Light Green T-Shirt","description":"Product Description","image":"light-green-t-shirt.jpg","price":49,"category":"Clothing","quantity":34,"inventoryStatus":"INSTOCK","rating":4},{"id":"1016","code":"k8l6j58jl","name":"Lime Band","description":"Product Description","image":"lime-band.jpg","price":79,"category":"Fitness","quantity":12,"inventoryStatus":"INSTOCK","rating":3},{"id":"1017","code":"v435nn85n","name":"Mini Speakers","description":"Product Description","image":"mini-speakers.jpg","price":85,"category":"Clothing","quantity":42,"inventoryStatus":"INSTOCK","rating":4},{"id":"1018","code":"09zx9c0zc","name":"Painted Phone Case","description":"Product Description","image":"painted-phone-case.jpg","price":56,"category":"Accessories","quantity":41,"inventoryStatus":"INSTOCK","rating":5},{"id":"1019","code":"mnb5mb2m5","name":"Pink Band","description":"Product Description","image":"pink-band.jpg","price":79,"category":"Fitness","quantity":63,"inventoryStatus":"INSTOCK","rating":4},{"id":"1020","code":"r23fwf2w3","name":"Pink Purse","description":"Product Description","image":"pink-purse.jpg","price":110,"category":"Accessories","quantity":0,"inventoryStatus":"OUTOFSTOCK","rating":4},{"id":"1021","code":"pxpzczo23","name":"Purple Band","description":"Product Description","image":"purple-band.jpg","price":79,"category":"Fitness","quantity":6,"inventoryStatus":"LOWSTOCK","rating":3},{"id":"1022","code":"2c42cb5cb","name":"Purple Gemstone Necklace","description":"Product Description","image":"purple-gemstone-necklace.jpg","price":45,"category":"Accessories","quantity":62,"inventoryStatus":"INSTOCK","rating":4},{"id":"1023","code":"5k43kkk23","name":"Purple T-Shirt","description":"Product Description","image":"purple-t-shirt.jpg","price":49,"category":"Clothing","quantity":2,"inventoryStatus":"LOWSTOCK","rating":5},{"id":"1024","code":"lm2tny2k4","name":"Shoes","description":"Product Description","image":"shoes.jpg","price":64,"category":"Clothing","quantity":0,"inventoryStatus":"INSTOCK","rating":4},{"id":"1025","code":"nbm5mv45n","name":"Sneakers","description":"Product Description","image":"sneakers.jpg","price":78,"category":"Clothing","quantity":52,"inventoryStatus":"INSTOCK","rating":4},{"id":"1026","code":"zx23zc42c","name":"Teal T-Shirt","description":"Product Description","image":"teal-t-shirt.jpg","price":49,"category":"Clothing","quantity":3,"inventoryStatus":"LOWSTOCK","rating":3},{"id":"1027","code":"acvx872gc","name":"Yellow Earbuds","description":"Product Description","image":"yellow-earbuds.jpg","price":89,"category":"Electronics","quantity":35,"inventoryStatus":"INSTOCK","rating":3},{"id":"1028","code":"tx125ck42","name":"Yoga Mat","description":"Product Description","image":"yoga-mat.jpg","price":20,"category":"Fitness","quantity":15,"inventoryStatus":"INSTOCK","rating":5},{"id":"1029","code":"gwuby345v","name":"Yoga Set","description":"Product Description","image":"yoga-set.jpg","price":20,"category":"Fitness","quantity":25,"inventoryStatus":"INSTOCK","rating":8}]'
       )
@@ -60,7 +60,7 @@ const OrdersList = () => {
   };
 
   const openNew = () => {
-    setProduct(emptyProduct);
+    setOrder(emptyProduct);
     setSubmitted(false);
     setProductDialog(true);
   };
@@ -79,35 +79,35 @@ const OrdersList = () => {
   };
 
   const onCategoryChange = (e) => {
-    let _product = { ...product };
+    let _product = { ...order };
     _product["category"] = e.value;
-    setProduct(_product);
+    setOrder(_product);
   };
 
   const onInputChange = (e, name) => {
     const val = (e.target && e.target.value) || "";
-    let _product = { ...product };
+    let _product = { ...order };
     _product[`${name}`] = val;
 
-    setProduct(_product);
+    setOrder(_product);
   };
 
   const onInputNumberChange = (e, name) => {
     const val = e.value || 0;
-    let _product = { ...product };
+    let _product = { ...order };
     _product[`${name}`] = val;
 
-    setProduct(_product);
+    setOrder(_product);
   };
 
   const saveProduct = () => {
     setSubmitted(true);
 
-    if (product.name.trim()) {
-      let _products = [...products];
-      let _product = { ...product };
-      if (product.id) {
-        const index = findIndexById(product.id);
+    if (order.name.trim()) {
+      let _products = [...orders];
+      let _product = { ...order };
+      if (order.id) {
+        const index = findIndexById(order.id);
 
         _products[index] = _product;
         toast.current.show({
@@ -127,27 +127,27 @@ const OrdersList = () => {
         });
       }
 
-      setProducts(_products);
+      setOrders(_products);
       setProductDialog(false);
-      setProduct(emptyProduct);
+      setOrder(emptyProduct);
     }
   };
 
   const editProduct = (product) => {
-    setProduct({ ...product });
+    setOrder({ ...product });
     setProductDialog(true);
   };
 
   const confirmDeleteProduct = (product) => {
-    setProduct(product);
+    setOrder(product);
     setDeleteProductDialog(true);
   };
 
   const deleteProduct = () => {
-    let _products = products.filter((val) => val.id !== product.id);
-    setProduct(_products);
+    let _products = orders.filter((val) => val.id !== order.id);
+    setOrder(_products);
     setDeleteProductDialog(false);
-    setProduct(emptyProduct);
+    setOrder(emptyProduct);
     toast.current.show({
       severity: "success",
       summary: "Successful",
@@ -158,8 +158,8 @@ const OrdersList = () => {
 
   const findIndexById = (id) => {
     let index = -1;
-    for (let i = 0; i < products.length; i++) {
-      if (products[i].id === id) {
+    for (let i = 0; i < orders.length; i++) {
+      if (orders[i].id === id) {
         index = i;
         break;
       }
@@ -187,10 +187,10 @@ const OrdersList = () => {
   };
 
   const deleteSelectedProducts = () => {
-    let _products = products.filter((val) => !selectedProducts.includes(val));
-    setProducts(_products);
+    let _products = orders.filter((val) => !selectedOrders.includes(val));
+    setOrders(_products);
     setDeleteProductsDialog(false);
-    setSelectedProducts(null);
+    setSelectedOrders(null);
     toast.current.show({
       severity: "success",
       summary: "Successful",
@@ -213,7 +213,7 @@ const OrdersList = () => {
           icon="pi pi-trash"
           className="p-button-danger"
           onClick={confirmDeleteSelected}
-          disabled={!selectedProducts || !selectedProducts.length}
+          disabled={!selectedOrders || !selectedOrders.length}
         />
       </React.Fragment>
     );
@@ -350,9 +350,9 @@ const OrdersList = () => {
 
         <DataTable
           ref={dt}
-          value={products}
-          selection={selectedProducts}
-          onSelectionChange={(e) => setSelectedProducts(e.value)}
+          value={orders}
+          selection={selectedOrders}
+          onSelectionChange={(e) => setSelectedOrders(e.value)}
           dataKey="id"
           paginator
           rows={10}
@@ -394,7 +394,7 @@ const OrdersList = () => {
       </div>
 
       <Dialog
-        visible={productDialog}
+        visible={orderDialog}
         style={{ width: "450px" }}
         header="Product Details"
         modal
@@ -406,12 +406,12 @@ const OrdersList = () => {
           <label htmlFor="name">Name</label>
           <InputText
             id="name"
-            value={product.name}
+            value={order.name}
             onChange={(e) => onInputChange(e, "name")}
             required
             autoFocus
           />
-          {submitted && !product.name && (
+          {submitted && !order.name && (
             <small className="p-invalid">Name is required.</small>
           )}
         </div>
@@ -419,7 +419,7 @@ const OrdersList = () => {
           <label htmlFor="description">Description</label>
           <InputTextarea
             id="description"
-            value={product.description}
+            value={order.description}
             onChange={(e) => onInputChange(e, "description")}
             required
             rows={3}
@@ -436,7 +436,7 @@ const OrdersList = () => {
                 name="category"
                 value="Accessories"
                 onChange={onCategoryChange}
-                checked={product.category === "Accessories"}
+                checked={order.category === "Accessories"}
               />
               <label htmlFor="category1">Accessories</label>
             </div>
@@ -446,7 +446,7 @@ const OrdersList = () => {
                 name="category"
                 value="Clothing"
                 onChange={onCategoryChange}
-                checked={product.category === "Clothing"}
+                checked={order.category === "Clothing"}
               />
               <label htmlFor="category2">Clothing</label>
             </div>
@@ -456,7 +456,7 @@ const OrdersList = () => {
                 name="category"
                 value="Electronics"
                 onChange={onCategoryChange}
-                checked={product.category === "Electronics"}
+                checked={order.category === "Electronics"}
               />
               <label htmlFor="category3">Electronics</label>
             </div>
@@ -466,7 +466,7 @@ const OrdersList = () => {
                 name="category"
                 value="Fitness"
                 onChange={onCategoryChange}
-                checked={product.category === "Fitness"}
+                checked={order.category === "Fitness"}
               />
               <label htmlFor="category4">Fitness</label>
             </div>
@@ -478,7 +478,7 @@ const OrdersList = () => {
             <label htmlFor="price">Price</label>
             <InputNumber
               id="price"
-              value={product.price}
+              value={order.price}
               onValueChange={(e) => onInputNumberChange(e, "price")}
               mode="currency"
               currency="USD"
@@ -489,7 +489,7 @@ const OrdersList = () => {
             <label htmlFor="quantity">Quantity</label>
             <InputNumber
               id="quantity"
-              value={product.quantity}
+              value={order.quantity}
               onValueChange={(e) => onInputNumberChange(e, "quantity")}
               integeronly
             />
@@ -498,7 +498,7 @@ const OrdersList = () => {
       </Dialog>
 
       <Dialog
-        visible={deleteProductDialog}
+        visible={deleteOrderDialog}
         style={{ width: "450px" }}
         header="Confirm"
         modal
@@ -510,16 +510,16 @@ const OrdersList = () => {
             className="pi pi-exclamation-triangle p-mr-3"
             style={{ fontSize: "2rem" }}
           />
-          {product && (
+          {order && (
             <span>
-              Are you sure you want to delete <b>{product.name}</b>?
+              Are you sure you want to delete <b>{order.name}</b>?
             </span>
           )}
         </div>
       </Dialog>
 
       <Dialog
-        visible={deleteProductsDialog}
+        visible={deleteOrdersDialog}
         style={{ width: "450px" }}
         header="Confirm"
         modal
@@ -531,7 +531,7 @@ const OrdersList = () => {
             className="pi pi-exclamation-triangle p-mr-3"
             style={{ fontSize: "2rem" }}
           />
-          {product && (
+          {order && (
             <span>Are you sure you want to delete the selected products?</span>
           )}
         </div>
